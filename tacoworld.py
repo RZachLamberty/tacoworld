1d1dbc4e9b405710aa58c0a07990762d34604f40
Eamon <eamon@toddler.com>
2020-09-15 04:53:57 +0000

    tacoworld: plenty o tacos

--- tacoworld.py
+++ tacoworld.py
@@ -1,2 +1,2 @@
 print("I, Zach Lamberty, want tacos more than *anyone*, especially more than Eamon!")
-print("I, Eamon Lamberty, want tacos!!")
+print("I, Eamon Lamberty, want tacos, so it's good there are enough to share!")
